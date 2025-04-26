f4b56546ca8b73baba6273a93a90d7fca5d2f004
Luis Saurith <sauriths@gmail.com>
2025-04-26 07:49:22 -0500

    Conflicto

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+dic = {
+    'a': [1, 2, 3],
+    'b': [4, 5, 6],
+    'c': [7, 8, 9]      
+}
+
+df = pd.DataFrame(dic)
+print(df)
+# df.plot()
+# plt.show()
